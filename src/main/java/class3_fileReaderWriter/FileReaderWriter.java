b4c109e885f88e0200b831644c4ab2fedbb1ff41
Elena Bezrodnova <lenyka_86@mail.ru>
2023-10-25 12:40:48 +0200

    solved tasks FileReader/Writer part2 morning session

--- src/main/java/class3_fileReaderWriter/FileReaderWriter.java
+++ src/main/java/class3_fileReaderWriter/FileReaderWriter.java
@@ -1,19 +1,17 @@
 package class3_fileReaderWriter;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class FileReaderWriter {
     //Exercise 1: Reading from a File
     public void displayTextFromFile() {
         try (FileReader fr = new FileReader("input.txt")) {
             int c;
-            while((c = fr.read()) != -1) {
+            while ((c = fr.read()) != -1) {
                 System.out.print((char) c);
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -22,9 +20,8 @@ public class FileReaderWriter {
     public void writeTextToFile() {
         String text = "Invest in yourself; learning is the key to endless opportunities.";
         try (FileWriter fw = new FileWriter("output.txt")) {
-                fw.write(text);
-        }
-        catch (IOException e) {
+            fw.write(text);
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -34,10 +31,10 @@ public class FileReaderWriter {
         try (FileReader fr = new FileReader("input.txt");
              FileWriter fw = new FileWriter("destination.txt")) {
             int c;
-            while((c = fr.read()) != -1) {
-                fw.write(c);          }
-        }
-        catch (IOException e) {
+            while ((c = fr.read()) != -1) {
+                fw.write((char) c);
+            }
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -48,8 +45,7 @@ public class FileReaderWriter {
 
         try (FileWriter fw = new FileWriter("output.txt", true)) {
             fw.write("\n" + text);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -61,7 +57,7 @@ public class FileReaderWriter {
             String line;
             List<Integer> integers = new ArrayList<>();
 
-            while((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
                 String[] temp = line.split(", ");
                 for (int i = 0; i < temp.length; i++) {
                     integers.add(Integer.parseInt(temp[i]));
@@ -69,21 +65,133 @@ public class FileReaderWriter {
             }
             System.out.println(integers);
             return integers;
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        return  null;
+        return null;
     }
+
     public void printNumbersFromFileSum() {
         List<Integer> numbers = readNumbersFromFile();
         int sum = numbers.stream().reduce(0, (acc, number) -> acc + number);
         try (FileWriter fw = new FileWriter("output_numbers.txt")) {
             fw.write(Integer.toString(sum));
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    //Second part
+    //Exercise: Text Collector with Uniqueness
+
+    public void saveUserInputIntoFile(Set<String> userInput) {
+        try (FileWriter fw = new FileWriter("output.txt", true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            for (String input : userInput) {
+                bw.write(input);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void displayUserInputFromFile() {
+        try (FileReader fr = new FileReader("output.txt");
+             BufferedReader br = new BufferedReader(fr)) {
+            String line;
+            boolean isFileEmpty = true;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                isFileEmpty = false;
+            }
+
+            if (isFileEmpty) {
+                System.out.println("File is empty");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void collectUserInputText() {
+        displayUserInputFromFile();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter something");
+
+        String input = sc.nextLine();
+        Set<String> userInput = new HashSet<>();
+
+        while (!input.equalsIgnoreCase("export")) {
+            userInput.add(input);
+            input = sc.nextLine();
+        }
+
+        saveUserInputIntoFile(userInput);
+    }
+
+    //Exercise 2: Line Counter
+    public Integer countLinesInFile() {
+        int count = 0;
+
+        try (FileReader fr = new FileReader("output.txt");
+             BufferedReader br = new BufferedReader(fr)) {
+
+            while (br.readLine() != null) {
+                count++;
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return count;
+    }
+
+    //Exercise 3: Convert to Uppercase
+    public void fileContentToUpperCase() {
+        try (FileReader fr = new FileReader("output.txt");
+             BufferedReader br = new BufferedReader(fr);
+             FileWriter fw = new FileWriter("output_ToUpperCase.txt", true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                bw.write(line.toUpperCase());
+                bw.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //Exercise 4: Duplicate Word Finder
+
+    public void printDuplicateWords() {
+        Map<String, Integer> wordsToCountMap = new HashMap<>();
+        try (FileReader fr = new FileReader("output.txt");
+             BufferedReader br = new BufferedReader(fr)) {
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                for (String word: line.split("\\s+|\\p{Punct}")) {
+                    if (wordsToCountMap.containsKey(word)) {
+                        wordsToCountMap.put(word, wordsToCountMap.get(word) + 1);
+                    }
+                    else {
+                        wordsToCountMap.put(word, 0);
+                    }
+                }
+            }
+            wordsToCountMap.entrySet().stream()
+                    .filter(elem -> elem.getValue() > 0)
+                    .forEach(elem -> System.out.println("Duplicate word: " + elem.getKey()));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/main/java/class3_fileReaderWriter/Main.java
+++ src/main/java/class3_fileReaderWriter/Main.java
@@ -3,11 +3,18 @@ package class3_fileReaderWriter;
 public class Main {
     public static void main(String[] args) {
         FileReaderWriter readerWriter = new FileReaderWriter();
-        readerWriter.displayTextFromFile();
-        readerWriter.writeTextToFile();
-        readerWriter.copyFileContent();
-        readerWriter.appendTextToFile();
-        readerWriter.readNumbersFromFile();
-        readerWriter.printNumbersFromFileSum();
+//        readerWriter.displayTextFromFile();
+//        readerWriter.writeTextToFile();
+//        readerWriter.copyFileContent();
+//        readerWriter.appendTextToFile();
+//        readerWriter.readNumbersFromFile();
+//        readerWriter.printNumbersFromFileSum();
+
+        System.out.println("Second part of the Day3");
+        System.out.println("******");
+        readerWriter.collectUserInputText();
+        System.out.println(readerWriter.countLinesInFile());
+        readerWriter.fileContentToUpperCase();
+        readerWriter.printDuplicateWords();
     }
 }
